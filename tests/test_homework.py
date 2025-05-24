90952ab317ed6edde6fde791062648ae379b291e
sqv <saquinterova@unal.edu.co>
2025-05-24 15:51:54 +0000

    Done

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -11,20 +11,13 @@ def test_01():
 
     # Test if the homework script runs without errors
     try:
-        for entry_point in ["elasticnet", "knn"]:
+        for model in ["elasticnet", "knn"]:
             subprocess.run(
-                [
-                    "mlflow",
-                    "run",
-                    "--env-manager=local",
-                    "-e",
-                    entry_point,
-                    ".",
-                ],
+                ["python3", "-m", "homework", "--model", model],
                 check=True,
             )
     except subprocess.CalledProcessError as e:
-        raise Exception(f"Error running the homework script: {e}")
+        raise Exception(f"Error running the homework script: {e}")	
 
     # Ensure the mlruns directory exists
     assert os.path.exists("my_mlruns"), "mlruns directory does not exist."
@@ -33,4 +26,5 @@ def test_01():
     experiments = [
         d for d in os.listdir("my_mlruns") if os.path.isdir(os.path.join("my_mlruns", d))
     ]
-    assert len(experiments) > 0, "No experiments found in my_mlruns directory."
+    assert len(experiments) > 0, "No experiments found in mlruns directory."
+
